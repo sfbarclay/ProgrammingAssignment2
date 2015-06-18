c8a45d9c169f33788aa3d6882d9a8c3eb949f151
sfbarclay <sfbarclay@yahoo.ca>
2015-06-18 10:22:10 -0600

    This is a test modification
     # Please enter the commit message for your
    changes. Lines starting # with '#' will be ignored, and an empty
    message aborts the commit.  # On branch master # Your branch is
    up-to-date with 'origin/master'.  # # Changes to be committed: #
    modified: cachematrix.R #

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Write a short comment describing this function - This is a test to see how to commit
 
 makeCacheMatrix <- function(x = matrix()) {
 
